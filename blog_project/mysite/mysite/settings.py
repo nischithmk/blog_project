51efb19b323c04dd04dd87774933ab3ee0d87cb1
Nischith M K <mknischith71@gmail.com>
2022-10-19 18:46:57 +0530

    added css

--- blog_project/mysite/mysite/settings.py
+++ blog_project/mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/templates/blog')
@@ -26,7 +26,7 @@ SECRET_KEY = 'rg6av!hufwp@p3b@)+c!3b=z)rr+^(om-gpw8usu!hswfcuy)9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,3 +128,6 @@ LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+django_heroku.settings(locals())
